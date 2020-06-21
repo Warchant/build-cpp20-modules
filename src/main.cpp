0fe5e7d7f894610b8bbc730fd3e7e2f001d95c93
Bohdan Vanieiev <warchantua@gmail.com>
2020-06-21 18:34:56 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+import module1;
+
+int main() {
+    module1::hello();
+    return 0;
+}
---
+++ src/module1.cpp
@@ -0,0 +1,11 @@
+export module module1;
+
+import <cstdio>;
+
+namespace module1 {
+
+    export void hello() {
+        puts("Hello from module1\n");
+    }
+
+}
